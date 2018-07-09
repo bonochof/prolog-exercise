4a1d537eb64e3ef6134ed3339b28ba3f239b39fd
bonochof <ryos427@yahoo.co.jp>
2018-07-09 16:42:38 +0900

    add DB

---
+++ DBsearch/DB.pl
@@ -0,0 +1,11 @@
+/*
+  family database
+  Ryosuke Takata
+*/
+
+family(
+  person(tom, fox, date(7, may, 1950), works(bbc, 15200)),
+  person(ann, fox, date(9, may, 1951), unemployed),
+  [person(pat, fox, date(5, may, 1973), unemployed),
+   person(jim, fox, date(5, may, 1973), unemployed)]).
+
