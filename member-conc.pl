90e78e2bb71ef6b6b422c0f71d93294db4ca4b30
bonochof <ryos427@yahoo.co.jp>
2018-07-09 16:17:03 +0900

    add conc exercise

---
+++ member-conc.pl
@@ -0,0 +1,12 @@
+/*
+  member-conc.pl
+  Ryosuke Takata
+*/
+
+conc([], L2, L2).
+conc([L1Head| L1Tail], L2, [L1Head| LoutTail]) :-
+  conc(L1Tail, L2, LoutTail).
+
+member(X, L) :-
+  conc(L1, [X|L2], L).
+
