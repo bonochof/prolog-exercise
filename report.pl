027776b7b97ceb582bfc623db450e5fd4b4c186f
bonochof <ryos427@yahoo.co.jp>
2018-07-17 16:39:25 +0900

    add length

--- report.pl
+++ report.pl
@@ -3,7 +3,16 @@
   Ryosuke Takata
 */
 
+%%% Base Predict %%%
 conc([], L2, L2).
 conc([L1Head| L1Tail], L2, [L1Head| LoutTail]) :-
   conc(L1Tail, L2, LoutTail).
 
+length([], 0).
+length([H|T], N) :-
+  length(T, N1),
+  N is 1 + N1.
+
+% ex) halflist([a, b, c, d, e], [a, b], [c, d, e]). -> yes
+halflist(L1, L2, L3) :-
+  conc(L2, L3, L1).
