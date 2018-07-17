d2f47b9dc6650ec18d1e31c5e5099e44e5637301
bonochof <ryos427@yahoo.co.jp>
2018-07-17 16:57:46 +0900

    add halflist

--- report.pl
+++ report.pl
@@ -8,12 +8,18 @@ conc([], L2, L2).
 conc([L1Head| L1Tail], L2, [L1Head| LoutTail]) :-
   conc(L1Tail, L2, LoutTail).
 
+/*
 length([], 0).
 length([H|T], N) :-
   length(T, N1),
   N is 1 + N1.
+*/
 
 %%% Report Predict %%%
 % ex) halflist([a, b, c, d, e], [a, b], [c, d, e]). -> yes
 halflist(L1, L2, L3) :-
-  conc(L2, L3, L1).
+  conc(L2, L3, L1),
+  length(L2, N2),
+  length(L3, N3),
+  (N3 is N2; N3 is N2 + 1).
+
