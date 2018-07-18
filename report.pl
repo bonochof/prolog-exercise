0eada2244f37a2f85fb76d3a72bc092f9241132a
bonochof <ryos427@yahoo.co.jp>
2018-07-18 13:01:21 +0900

    add all comment

--- report.pl
+++ report.pl
@@ -40,3 +40,43 @@ flatten(L, Flatlist).
 rotate_right(L1, [L2Head| L2Tail]) :-
   conc(L2Tail, [L2Head], L1).
 
+% (5) insert(X, L, L2).
+%     ex) insert(new, [a, b, c], [new, a, b, c]). -> yes
+%         insert(new, [a, b, c], [a, new, b, c]). -> yes
+%         insert(new, [a, b, c], [a, b, new, c]). -> yes
+%         insert(new, [a, b, c], [a, b, c, new]). -> yes
+
+% (6) sublist(L1, L2).
+%     ex) sublist([a, b, c, d], [b, c]). -> yes
+%         sublist([a, b, c, d], []). -> yes
+%         sublist([a, b, c, d], [b, d]). -> no
+
+% (7) subset(Set, Subset).
+%     ex) subset([a, b, c], S).
+%         -> S=[a,b,c]; [b,c]; [a,c]; [a,b]; [a]; [b]; [c]; []
+
+% (8) reverse(L1, L2).
+%     ex) reverse([a, b, c, d], [d, c, b, a]). -> yes
+
+% (9) max-num(L1, N).
+%     ex) max-num([4, 5, 2, 1], 5). -> yes
+
+% (10) total-num(L1, N).
+%      ex) total-num([4, 5, 2, 1], 12). -> yes
+
+% (11) list-ave(L1, N).
+%      ex) list-ave([1, 4, 4], 3). -> yes
+
+% (12) kaibun(List).
+%      ex) kaibun([a, b, c]). -> no
+%          kaibun([ta, ke, ya, bu, ya, ke, ta]). -> yes
+
+% (13) assoc-pro(Key, Alist, Result).
+%      ex) assoc-pro(b, [[a, x], [b, y], [c, z]], [b, y]). -> yes
+
+% (14) translate(L1, L2).
+%      ex) translate([dog, cat, monkey], [inu, neko, saru]). -> yes
+means(dog, inu).
+means(cat, neko).
+means(monkey, saru).
+
